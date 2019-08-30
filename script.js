eafe90452b436d08f5bfbc3be2b61ec56c701720
Mitchell <47989170+VerumEmanio@users.noreply.github.com>
2019-08-30 12:05:24 -0400

    Added event listeners for nav buttons

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.addEventListener('click', '.home', {
 
+document.querySelector('.home').addEventListener('click', function() { // Do these lines violate DRY?
+    window.open('index.html');
+});
+
+document.querySelector('.menu').addEventListener('click', function() {
+    window.open('menu.html');
+});
+
+document.querySelector('.events').addEventListener('click', function() {
+    window.open('eventsIndex.html');
+});
+
+document.querySelector('.about').addEventListener('click', function() {
+    window.open('about.html');
 });
